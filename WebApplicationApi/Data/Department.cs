904ec44d1c7027506d598bfd79deec636eb1de55
Hieu Nguyen <hieunguyenbk93@gmail.com>
2023-04-03 14:58:11 +0700

    Add project files.

---
+++ WebApplicationApi/Data/Department.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationApi.Data
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Byte Level { get; set; }
+        public int ParentId { get; set; }
+        public Boolean Status { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ WebApplicationApi/Data/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationApi.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> opt): base(opt)
+        {
+
+        }
+    }
+}
